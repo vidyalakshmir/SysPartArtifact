21216d31631710099ef7cde2b313252a72c44eff
vidya <vidyalakshmi.raj@gmail.com>
2023-10-26 14:36:50 -0400

    Updated dynamic lib analysis

--- analysis/app/src/dlanalysis/static/run_dlanalysis.sh
+++ analysis/app/src/dlanalysis/static/run_dlanalysis.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 #src/dlanalysis/static/run_dlanalysis.sh src/dlanalysis/binaries 	
 
-BIN_PATH=$1
-TYPEARMOR_PATH=$2
+#BIN_PATH=$1
+OUT_PATH=../../../../../outputs
 
-BINARY=(redis-server nginx httpd lighttpd memcached named)
-for i in "${BINARY[@]}" 
+BINARY_NAME=(redis-server nginx httpd lighttpd memcached named)
+BINARY=("$BIND","$HTTPD","$LIGHTTPD","$MEMCACHED","$NGINX","$REDIS")
+for i in "${BINARY_NAME[@]}" 
 do
 	echo "$i"
-	DLOUT="$TYPEARMOR_PATH/$i/dloutput"
+	DLOUT="$OUT_PATH/$i/dloutput"
 	echo "mkdir -p $DLOUT"
 	mkdir -p $DLOUT
 	
-	echo "./syspart -p $BIN_PATH/$i -i -t $TYPEARMOR_PATH/$i/typearmor/typearmor_parsed.txt -s main -a 6,dlopen@@GLIBC_2.2.5,7"
-	./syspart -p $BIN_PATH/$i -i -t $TYPEARMOR_PATH/$i/typearmor/typearmor_parsed.txt -s main -a 6,dlopen@@GLIBC_2.2.5,7 > $DLOUT/dlopen_static.txt && echo OK
+	echo "./syspart -p $BINARY -i -t $OUT_PATH/$i/typearmor/typearmor_parsed.txt -s main -a 6,dlopen@@GLIBC_2.2.5,7"
+	./syspart -p $BINARY -i -t $OUT_PATH/$i/typearmor/typearmor_parsed.txt -s main -a 6,dlopen@@GLIBC_2.2.5,7 > $DLOUT/dlopen_static.txt && echo OK
 	
-	echo "./syspart -p $BIN_PATH/$i -i -t $TYPEARMOR_PATH/$i/typearmor/typearmor_parsed.txt -s main -a 6,dlsym,6"
-	./syspart -p $BIN_PATH/$i -i -t $TYPEARMOR_PATH/$i/typearmor/typearmor_parsed.txt -s main -a 6,dlsym,6 > $DLOUT/dlsym_static.txt && echo OK
+	echo "./syspart -p $BINARY -i -t $OUT_PATH/$i/typearmor/typearmor_parsed.txt -s main -a 6,dlsym,6"
+	./syspart -p $BINARY -i -t $OUT_PATH/$i/typearmor/typearmor_parsed.txt -s main -a 6,dlsym,6 > $DLOUT/dlsym_static.txt && echo OK
 	
 	src/dlanalysis/static/generate_libnames.sh $i
 	echo "src/dlanalysis/static/match_libs_with_syms.sh $DLOUT/dlsym_static.txt > $DLOUT/libraries_matching_syms.txt"
